369cb0f4301419a32c199d67e5e85ed190c6e4e3
Deadshot0x7 <sviquarahmed@gmail.com>
2022-03-17 14:37:59 +0530

    Added
    Added the Logs

--- bot.py
+++ bot.py
@@ -3,18 +3,21 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 import time as t
 import os as s 
 from response import *
+import logging 
 
-
-
+PORT=int(s.environ.get('PORT','5000'))
+TOKEN = '1849563775:AAGGX0GCema6OelGZprpu6AdwjzyoWfeULQ'
+updater.bot.setWebhook('https://mahatbtbot.herokuapp.com/' + TOKEN)
 t.sleep(2)
 print("Bot has been started")
+logging.info("the bot has been intailised ")
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(CommandHandler('bye',b))
 updater.dispatcher.add_handler(CommandHandler('chatbot',chat))
 updater.dispatcher.add_handler(CommandHandler('start',start))
 updater.start_polling()
 updater.idle()
-port = int(os.environ["0.0.0.0"])
+
 
 
 
